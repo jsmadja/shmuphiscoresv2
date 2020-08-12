3d979d24bd1d9ab47c796ebfc2e0adb19a41d4ce
jsmadja <xedy4bsa>
2020-08-12 18:28:49 +0200

    Remove About page

--- src/router/index.ts
+++ src/router/index.ts
@@ -47,15 +47,6 @@ const routes: Array<RouteConfig> = [
     component: MyRecommendations
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  {
     path: "/players",
     name: "All Players",component: () => import("../views/Players.vue")
   }
