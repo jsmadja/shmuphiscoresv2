a0e95308e23bc688aaf3ebdfffcfaa3598da8fd9
jsmadja <xedy4bsa>
2020-08-17 20:04:42 +0200

    Platforms.vue

--- src/views/Platforms.vue
+++ src/views/Platforms.vue
@@ -5,6 +5,21 @@
         <h1>Platforms</h1>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title>
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
     <v-row dense>
       <v-col v-for="(item, i) in items" :key="i" xs="12" sm="12" md="12" lg="4">
         <v-card to="/platform">
