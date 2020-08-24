a9621b94253c9e4259aa64d698ccebcb5682342c
jsmadja <julien.smadja@gmail.Com>
2020-08-24 17:15:03 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import HighchartsVue from "highcharts-vue";
 import formatNumber from "./filters/formatNumber";
+import * as Sentry from "@sentry/browser";
+import { Vue as VueIntegration } from "@sentry/integrations";
+
+Sentry.init({
+  dsn: "https://bf7fe4a7fe794de8a5cbd53a3870468b@o438398.ingest.sentry.io/5403013",
+  integrations: [new VueIntegration({ Vue, attachProps: true })],
+});
 
 Vue.config.productionTip = false;
 
