e79cdf868d4b60d0405e76dd54f4454f1d29176c
jsmadja <xedy4bsa>
2020-09-03 17:47:43 +0200

    Layout

--- src/components/templates/CompareScoresTemplate.vue
+++ src/components/templates/CompareScoresTemplate.vue
@@ -5,73 +5,86 @@
         <h1>Compare Scores</h1>
       </v-col>
     </v-row>
-    <v-card>
-      <v-card-text>
-        <v-row>
-          <v-col cols="12" sm="6">
-            <v-autocomplete
-              :items="players"
-              v-model="selectedPlayer1"
-              item-value="id"
-              item-text="name"
-              hide-no-data
-              hide-selected
-              hide-details
-              label="Player 1"
-              placeholder="Start typing to Search"
-              outlined
-              @change="() => $emit('selectPlayer1', this.selectedPlayer1)"
-            ></v-autocomplete>
-          </v-col>
-          <v-col cols="12" sm="6">
-            <v-autocomplete
-              :items="players"
-              item-value="id"
-              item-text="name"
-              v-model="selectedPlayer2"
-              hide-no-data
-              hide-selected
-              hide-details
-              label="Player 2"
-              placeholder="Start typing to Search"
-              outlined
-              @change="() => $emit('selectPlayer2', this.selectedPlayer2)"
-            ></v-autocomplete>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col xs12>
-            <v-data-table
-              :headers="headers"
-              :items="versus"
-              :search="search"
-              :sort-by="['scoreGap']"
-              :sort-desc="[true]"
-              @click:row="(row) => $emit('goToGame', row)"
-            >
-              <template v-slot:item.score1.rank="{ item }">
-                <span
-                  :class="{ win: item.wonByPlayer1, lose: !item.wonByPlayer1 }"
-                  >{{ item.score1.rank | formatRank }}</span
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-text>
+            <v-row>
+              <v-col cols="12" sm="6">
+                <v-autocomplete
+                  :items="players"
+                  v-model="selectedPlayer1"
+                  item-value="id"
+                  item-text="name"
+                  hide-no-data
+                  hide-selected
+                  hide-details
+                  label="Player 1"
+                  placeholder="Start typing to Search"
+                  outlined
+                  @change="() => $emit('selectPlayer1', this.selectedPlayer1)"
+                ></v-autocomplete>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-autocomplete
+                  :items="players"
+                  item-value="id"
+                  item-text="name"
+                  v-model="selectedPlayer2"
+                  hide-no-data
+                  hide-selected
+                  hide-details
+                  label="Player 2"
+                  placeholder="Start typing to Search"
+                  outlined
+                  @change="() => $emit('selectPlayer2', this.selectedPlayer2)"
+                ></v-autocomplete>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col xs12>
+                <v-data-table
+                  :headers="headers"
+                  :items="versus"
+                  :search="search"
+                  :sort-by="['scoreGap']"
+                  :sort-desc="[true]"
+                  @click:row="(row) => $emit('goToGame', row)"
                 >
-              </template>
-              <template v-slot:item.score2.rank="{ item }">
-                <span
-                  :class="{ win: !item.wonByPlayer1, lose: item.wonByPlayer1 }"
-                  >{{ item.score2.rank | formatRank }}</span
-                >
-              </template>
-              <template v-slot:item.scoreGap="{ item }">
-                <span
-                  :class="{ win: item.wonByPlayer1, lose: !item.wonByPlayer1 }"
-                  >{{ item.scoreGap }}%</span
-                >
-              </template>
-            </v-data-table>
-          </v-col>
-        </v-row>
-      </v-card-text>
-    </v-card>
+                  <template v-slot:item.score1.rank="{ item }">
+                    <span
+                      :class="{
+                        win: item.wonByPlayer1,
+                        lose: !item.wonByPlayer1,
+                      }"
+                      >{{ item.score1.rank | formatRank }}</span
+                    >
+                  </template>
+                  <template v-slot:item.score2.rank="{ item }">
+                    <span
+                      :class="{
+                        win: !item.wonByPlayer1,
+                        lose: item.wonByPlayer1,
+                      }"
+                      >{{ item.score2.rank | formatRank }}</span
+                    >
+                  </template>
+                  <template v-slot:item.scoreGap="{ item }">
+                    <span
+                      :class="{
+                        win: item.wonByPlayer1,
+                        lose: !item.wonByPlayer1,
+                      }"
+                      >{{ item.scoreGap }}%</span
+                    >
+                  </template>
+                </v-data-table>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
--- src/components/templates/MyProfileTemplate.vue
+++ src/components/templates/MyProfileTemplate.vue
@@ -6,65 +6,75 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="12">
-        <v-switch
-          v-model="user"
-          input-value="hideMedals"
-          false-value="true"
-          label="Show medals on shmup.com"
-        ></v-switch>
-      </v-col>
-      <v-col cols="12">
-        <h3>Share my medals on forums</h3>
-      </v-col>
-      <v-col cols="6" xs="6" sm="4" md="3" lg="3">
-        <v-card light tile>
-          <v-img src="https://hiscores.shmup.com/player/33489/medals.png" />
-        </v-card>
-      </v-col>
-      <v-col cols="12" xs="12" sm="8" md="9" lg="9">
-        <v-card flat outlined>
-          <copyable-code
-            title="Code"
-            :code="`[IMG]https://hiscores.shmup.com/player/${user.shmupUserId}/medals.png[/IMG]`"
-            @copy="$emit('copy')"
-          />
-        </v-card>
-      </v-col>
-      <v-col cols="12"><h3>Share my last score on forums</h3></v-col>
       <v-col>
-        <v-card light tile>
-          <v-img
-            position="top left"
-            src="https://hiscores.shmup.com/player/1/signature.png"
-          />
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-card flat outlined>
-          <copyable-code
-            title="Code"
-            :code="`[IMG]https://hiscores.shmup.com/player/${user.id}/signature.png[/IMG]`"
-            @copy="$emit('copy')"
-          />
-        </v-card>
-      </v-col>
-      <v-col cols="12"><h3>Share my arch enemy</h3></v-col>
-      <v-col>
-        <v-card light tile>
-          <v-img
-            position="top left"
-            src="http://hiscores.shmup.com/player/1/versus.png"
-          />
-        </v-card>
-      </v-col>
-      <v-col>
-        <v-card flat outlined>
-          <copyable-code
-            title="Code"
-            :code="`[IMG]https://hiscores.shmup.com/player/${user.id}/versus.png[/IMG]`"
-            @copy="$emit('copy')"
-          />
+        <v-card>
+          <v-card-text>
+            <v-row>
+              <v-col cols="12">
+                <v-switch
+                  v-model="user"
+                  input-value="hideMedals"
+                  false-value="true"
+                  label="Show medals on shmup.com"
+                ></v-switch>
+              </v-col>
+              <v-col cols="12">
+                <h3>Share my medals on forums</h3>
+              </v-col>
+              <v-col cols="6" xs="6" sm="4" md="3" lg="3">
+                <v-card light tile>
+                  <v-img
+                    src="https://hiscores.shmup.com/player/33489/medals.png"
+                  />
+                </v-card>
+              </v-col>
+              <v-col cols="12" xs="12" sm="8" md="9" lg="9">
+                <v-card flat outlined>
+                  <copyable-code
+                    title="Code"
+                    :code="`[IMG]https://hiscores.shmup.com/player/${user.shmupUserId}/medals.png[/IMG]`"
+                    @copy="$emit('copy')"
+                  />
+                </v-card>
+              </v-col>
+              <v-col cols="12"><h3>Share my last score on forums</h3></v-col>
+              <v-col>
+                <v-card light tile>
+                  <v-img
+                    position="top left"
+                    src="https://hiscores.shmup.com/player/1/signature.png"
+                  />
+                </v-card>
+              </v-col>
+              <v-col>
+                <v-card flat outlined>
+                  <copyable-code
+                    title="Code"
+                    :code="`[IMG]https://hiscores.shmup.com/player/${user.id}/signature.png[/IMG]`"
+                    @copy="$emit('copy')"
+                  />
+                </v-card>
+              </v-col>
+              <v-col cols="12"><h3>Share my arch enemy</h3></v-col>
+              <v-col>
+                <v-card light tile>
+                  <v-img
+                    position="top left"
+                    src="http://hiscores.shmup.com/player/1/versus.png"
+                  />
+                </v-card>
+              </v-col>
+              <v-col>
+                <v-card flat outlined>
+                  <copyable-code
+                    title="Code"
+                    :code="`[IMG]https://hiscores.shmup.com/player/${user.id}/versus.png[/IMG]`"
+                    @copy="$emit('copy')"
+                  />
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-card-text>
         </v-card>
       </v-col>
     </v-row>
