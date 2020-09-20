93ff2b4faefa8af598b16c1757eb923db8580434
jsmadja <xedy4bsa>
2020-09-20 13:52:11 +0200

    Remove Sentry

--- src/main.ts
+++ src/main.ts
@@ -10,15 +10,7 @@ import {
   formatTime,
   formatDateFromNow,
 } from "./filters/index";
-import * as Sentry from "@sentry/browser";
-import { Vue as VueIntegration } from "@sentry/integrations";
 import VueClipboard from "vue-clipboard2";
-
-Sentry.init({
-  dsn:
-    "https://bf7fe4a7fe794de8a5cbd53a3870468b@o438398.ingest.sentry.io/5403013",
-  integrations: [new VueIntegration({ Vue, attachProps: true })],
-});
 
 Vue.config.productionTip = false;
 
