c5b997631e5355a34a558ad3299ad315a2e85c86
jsmadja <xedy4bsa>
2021-04-15 21:16:27 +0200

    Set full API url

--- src/api.ts
+++ src/api.ts
@@ -1,2 +1,4 @@
 export const api =
-  process.env.NODE_ENV === "development" ? "http://localhost:8080/api" : "/api";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8080/api"
+    : "https://hiscores.shmup.com/api";
